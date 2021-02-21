5ee94369923b79ecbe6c870baa101ba5a23ca18f
dalissongabriel <alissongabrieldino@gmail.com>
2021-02-21 16:30:49 -0300

    Estrutura de dados: tipagem dinamica

---
+++ estrutura_dados/tipagemDinamica.js
@@ -0,0 +1,14 @@
+// TIPAGEM DINÂMICA
+// Como pode ver abaixo, o tipo da váriavel "exemplo" muda em tempo de execução
+
+let exemplo = "43"
+//  "String"
+console.log(typeof exemplo)
+
+exemplo = 34
+// Number
+console.log(typeof exemplo)
+
+exemplo = true
+// Boolean
+console.log(typeof exemplo)
